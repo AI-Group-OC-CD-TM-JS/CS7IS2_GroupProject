ee0fd990c373f8704408935c54905105fd52b9b2
tom337 <tom@rgrid.tech>
2022-04-03 21:53:38 +0100

    Simulated Annealing script

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # CS7IS2_GroupProject
 Group Project for Artificial Intelligence CS7IS2
 
+# Info
+
+Simulated Annealing is contained from its own notebook, though the functionality itself is kept it a script so that we can run everything together and compare metrics.
+
+
 Team Members: Oscar Crowley, Conor Dalton, Tom Mulligan, and Johnny Scanlon
---
+++ SA/SA.py
@@ -0,0 +1,68 @@
+from typing import List, Tuple
+from scipy import spatial
+import numpy as np
+import matplotlib.pyplot as plt
+from sko.SA import SA_TSP
+import time
+
+DISTANCE_MATRIX = np.zeros
+
+
+def cal_total_distance(routine):
+    """Get the total distance between all nodes"""
+    (num_points,) = routine.shape
+    return sum(
+        [
+            DISTANCE_MATRIX[routine[i % num_points], routine[(i + 1) % num_points]]
+            for i in range(num_points)
+        ]
+    )
+
+
+def run_SA(
+    coordinates: np.array, iterations: int, t_max: int, t_min: int
+) -> Tuple[float, Tuple[List]]:
+    """
+    Run Simulated Annealing on a set of coords and their distance matrix.
+
+    Parameters
+    ----------
+    coordinates : np.array
+                A numpy array of coordinates. Expecting coordinates in the form that is produced
+                by the attention module. (3D array)
+    iterations : int
+                Number of iterations for SA
+    t_max : int
+            Temperature to being SA at.
+    t_min : int
+            Cut-off point for temperature
+
+    Returns
+    -------
+    best_distance : float
+        The best distance found
+    best_points : List[int]
+        The best order of nodes follow
+    """
+
+    global DISTANCE_MATRIX
+    DISTANCE_MATRIX = spatial.distance.cdist(
+        coordinates[0], coordinates[0], metric="euclidean"
+    )
+
+    # Initialise SA, using euclidean distance as objective function
+    sa_tsp = SA_TSP(
+        func=cal_total_distance,
+        x0=range(len(coordinates[0])),
+        T_max=t_max,
+        T_min=t_min,
+        L=iterations,
+    )
+
+    # Run SA and report total time as well as final distance
+    start_time = time.time()
+    best_points, best_distance = sa_tsp.run()
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    return best_distance, best_points
